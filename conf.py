35a126c5d399e032b8b31fbd2681f369bccf3682
Jesús García Crespo <jesus@sevein.com>
2023-10-31 13:18:16 +0000

    Enable plantuml jar location config

--- conf.py
+++ conf.py
@@ -4,6 +4,8 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -54,3 +56,9 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for PlantUML for Sphinx ----------------------------------------
+
+plantuml_jar = os.getenv("PLANTUML_JAR")
+if plantuml_jar:
+  plantuml = f"java -jar {plantuml_jar}"
